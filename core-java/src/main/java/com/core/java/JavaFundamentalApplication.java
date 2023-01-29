c687a338aabceae8d4405925db1068fc4ae1c983
Rakesh Kumar Sharma <65587975+sharmarkara@users.noreply.github.com>
2023-01-29 13:07:30 +0530

    Initial Structure

---
+++ core-java/src/main/java/com/core/java/JavaFundamentalApplication.java
@@ -0,0 +1,14 @@
+package com.core.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaFundamentalApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(JavaFundamentalApplication.class, args);
+	}
+
+}
---
+++ core-java/src/test/java/com/core/java/JavaFundamentalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.core.java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaFundamentalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
